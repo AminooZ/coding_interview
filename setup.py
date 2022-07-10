e8600a7d57f40c29c92f61a9210ed608d0a0714b
Amine <aminezghal@gmail.com>
2022-07-10 14:30:02 +0200

    update python and library versions

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     zip_safe=False,
-    python_requires='>=3.6.*',
+    python_requires='>=3.10.*',
     install_requires=[
-        'bitarray==1.0.1'
+        'bitarray==2.5.1'
     ]
 )
