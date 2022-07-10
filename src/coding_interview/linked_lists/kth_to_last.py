51478ce41f1c8a5d87485897d2db75f19f1ebe3b
Amine <aminezghal@gmail.com>
2022-07-10 14:44:34 +0200

    add edge cases

--- src/coding_interview/linked_lists/kth_to_last.py
+++ src/coding_interview/linked_lists/kth_to_last.py
@@ -4,10 +4,15 @@ from coding_interview.linked_lists.linked_lists import LinkedList
 
 
 def kth_to_last(linked_list: LinkedList, k: int) -> int:
+    if linked_list is None:
+        return None
     fast_pointer = linked_list.head
     slow_pointer = linked_list.head
     for _ in range(k):
-        fast_pointer = fast_pointer.next
+        if fast_pointer.next:
+            fast_pointer = fast_pointer.next
+        else:
+            return None
         # fast_pointer is k positions ahead of slow_pointer
     while fast_pointer.next:
         fast_pointer = fast_pointer.next
--- tests/coding_interview/linked_lists/test_kth_to_last.py
+++ tests/coding_interview/linked_lists/test_kth_to_last.py
@@ -10,6 +10,11 @@ from tests.coding_interview.linked_lists.test_linked_lists import \
     (LinkedList.from_list([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), 0, 0),
     (LinkedList.from_list([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), 3, 3),
     (LinkedList.from_list([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), 7, 7),
+    (LinkedList.from_list([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), 10, 10),
+    (LinkedList.from_list([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), 11, None),
+    (LinkedList.from_list([0]), 0, 0),
+    (LinkedList.from_list([0]), 1, None),
+    (None, 10, None)
 ])
 def test_kth_to_last(linked_list, k, expected):
     assert kth_to_last(linked_list=linked_list, k=k) == expected
